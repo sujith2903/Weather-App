46435aecee58b3b14e291345d257060c02c3a6e9
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-06-28 20:05:56 -0500

    Fetch condition & temperature

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const form = document.querySelector('form')
+const input = document.querySelector('form')
 const locations = document.querySelector('#location')
+const condition = document.querySelector('.condition')
+const city = document.querySelector('.city')
+const temperature = document.querySelector('.temperature')
 
-form.addEventListener('submit', (event) => {
+input.addEventListener('submit', (event) => {
     event.preventDefault()
 })
 
-locations.addEventListener('change', (event) => {
-    console.log(event.target.value)
+locations.addEventListener('change', () => {
+    getWeatherData()
 })
+
+async function getWeatherData() {
+    
+    console.log(locations.value)
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=8b5d56e9eee24f158d7225609232506&q=' + `${locations.value}`)
+    
+    const data = await response.json()
+
+    console.log(data)
+
+    condition.textContent = data.current.condition.text
+
+    city.textContent = `${ data.location.name }` + ', ' + `${data.location.region}` + ', ' + `${data.location.country}`
+
+    temperature.textContent = `${data.current.temp_f}` + ' °F'
+}
+
+getWeatherData()
